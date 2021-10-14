8b75b2fbf89052a128adafd0257d2db065383a72
H <hsilvap@me.com>
2021-10-14 13:29:08 -0500

    add async repo

--- interface/controller/pokemon_controller.go
+++ interface/controller/pokemon_controller.go
@@ -36,7 +36,7 @@ func (this *pokemonController) Get() gin.HandlerFunc {
 
 func (this *pokemonController) GetAsync() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var uri ConcurrentUriQueryParams
+		var uri PokemonAsyncUriQueryParams
 		if ctx.ShouldBindQuery(&uri) == nil {
 			if uri.Type != "odd" && uri.Type != "even" {
 				ctx.JSON(http.StatusBadRequest, gin.H{"msg": "Type value should be even or odd"})
@@ -44,9 +44,15 @@ func (this *pokemonController) GetAsync() gin.HandlerFunc {
 				ctx.JSON(http.StatusBadRequest, gin.H{"msg": "Items value should be greater than 0"})
 			} else if uri.ItemsPerWorker <= 0 {
 				ctx.JSON(http.StatusBadRequest, gin.H{"msg": "Items per worker value should be greater than 0"})
+			} else if uri.ItemsPerWorker > uri.Items {
+				ctx.JSON(http.StatusBadRequest, gin.H{"msg": "Items value should be greater than Items per worker"})
 			} else {
-				var data = this.repo.GetAsync(uri)
-				ctx.JSON(http.StatusOK, gin.H{"length": len(data), "data": data})
+				pkmn, err := this.repo.GetAsync(uri)
+				if err != nil {
+					ctx.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+				} else {
+					ctx.JSON(http.StatusOK, gin.H{"length": len(pkmn), "data": pkmn})
+				}
 			}
 
 		} else {
--- interface/repository/pokemon_repository.go
+++ interface/repository/pokemon_repository.go
@@ -2,8 +2,12 @@ package repository
 
 import (
 	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
 	"path/filepath"
 	"strconv"
+	"sync"
 
 	. "bootcamp/domain/model"
 	. "bootcamp/interface/services"
@@ -13,6 +17,7 @@ type pokemonRepository struct {
 }
 type PokemonRepository interface {
 	GetAll() ([]Pokemon, error)
+	GetAsync(params PokemonAsyncUriQueryParams) ([]*Pokemon, error)
 }
 
 //Pokemon Repository instance
@@ -39,12 +44,77 @@ func (p pokemonRepository) GetAll() ([]Pokemon, error) {
 	var result = make([]Pokemon, 0, 0)
 
 	for _, record := range records {
-		id, _ := strconv.Atoi(record[0])
-		pkmn := Pokemon{
-			Id:   id,
-			Name: record[1],
-		}
-		result = append(result, pkmn)
+		pokeInstance := Pokemon{}
+		result = append(result, *pokeInstance.ParseToPokemon(record))
 	}
 	return result, nil
+}
+
+//Reads pokemon using a worker pool
+func (p pokemonRepository) GetAsync(params PokemonAsyncUriQueryParams) ([]*Pokemon, error) {
+	f, _ := os.Open(filepath.Join(pkmnpath, filepath.Base(pkmnfilename)))
+	fcsv := csv.NewReader(f)
+	rs := make([]*Pokemon, 0)
+	numWps := params.Items / params.ItemsPerWorker
+	jobs := make(chan []string, numWps)
+	res := make(chan *Pokemon)
+	var wg sync.WaitGroup
+
+	for w := 0; w < numWps; w++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			worker(jobs, res, params)
+		}()
+	}
+
+	go func() {
+		for {
+			rStr, err := fcsv.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Println("ERROR: ", err.Error())
+				break
+			}
+			jobs <- rStr
+		}
+		close(jobs)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(res)
+	}()
+
+	for r := range res {
+		rs = append(rs, r)
+	}
+
+	return rs, nil
+}
+
+func worker(jobs <-chan []string, results chan<- *Pokemon, params PokemonAsyncUriQueryParams) {
+	totalLines := 0
+	for {
+		select {
+		case job, ok := <-jobs:
+			if !ok {
+				return
+			}
+			if totalLines == params.ItemsPerWorker {
+				break
+			}
+			id, _ := strconv.ParseUint(job[0], 10, 32)
+			pokeInstance := Pokemon{}
+			if params.Type == "even" && id%2 == 0 {
+				results <- pokeInstance.ParseToPokemon(job)
+				totalLines++
+			} else if params.Type == "odd" && id%2 == 1 {
+				results <- pokeInstance.ParseToPokemon(job)
+				totalLines++
+			}
+		}
+	}
 }
