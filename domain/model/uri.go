76450be8f62040d38c6404abc40337bed8c029b2
H <hsilvap@me.com>
2021-10-14 13:28:57 -0500

    Rename

--- domain/model/uri.go
+++ domain/model/uri.go
@@ -1,6 +1,6 @@
 package model
 
-type ConcurrentUriQueryParams struct {
+type PokemonAsyncUriQueryParams struct {
 	Type           string `form:"type" binding:"required"`
 	Items          int    `form:"items" binding:"required"`
 	ItemsPerWorker int    `form:"items_per_workers" binding:"required"`
